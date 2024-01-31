896622749f57efff80534c686dfb56395e5fefe2
Bin <45880588+Alicebond@users.noreply.github.com>
2024-01-31 17:43:15 -0330

    fix a bug and move dotenv from dependencies to devDependencies

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(limiter);
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
-      "script-src": ["'self'", "code.jquery.com", "cdn,jsdelivr.net"],
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
     },
   })
 );
